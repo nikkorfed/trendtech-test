10cbb440032c5ff2bf335c161da763fe09224302
Никита Корнилов <nikkorfed@gmail.com>
2021-08-06 10:57:08 +0500

    Add new transform function.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let array = [1, 2, 3, 4, 5, 6, 7, 8];
 let array2 = [1, 3, 4, 5, 6, 7, 8];
 let array3 = [1, 3, 4, 5, 6, 7, 8, 10, 11, 12];
 
-let transform = (array) =>
+let transform2 = (array) =>
   array.reduce((string, item, index) => {
     let previous = array[index - 1];
     let next = array[index + 1];
@@ -21,8 +21,20 @@ let transform = (array) =>
     }
   }, "");
 
-// Способ 2
-// Проходить по всем элементам тем же reduce и формировать массив массивов с последовательными числами. После пройтись по этим массивам и взять только крайние числа. После, объединить запятыми.
+let transform = (array) => {
+  let result = array.reduce((array, item) => {
+    if (!array.length) return array.push([item]) && array;
+
+    let lastSequence = array[array.length - 1];
+    let lastItem = lastSequence[lastSequence.length - 1];
+
+    lastItem + 1 == item ? lastSequence.push(item) : array.push([item]);
+    return array;
+  }, []);
+
+  let sequenceToString = (array) => (array.length > 2 ? `${array[0]}-${array[array.length - 1]}` : array.join(","));
+  return result.map(sequenceToString).join(",");
+};
 
 // let result = transform(array3);
 // console.log(result);
